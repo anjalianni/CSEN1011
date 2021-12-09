47b56040c13de34be01b36e7ad6029bf00f939d3
Giduturi Sai Sri Anjali <93367806+anjalianni@users.noreply.github.com>
2021-12-09 14:38:51 +0530

    Add files via upload

---
+++ fibnocci new.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+ {
+ 	  int num,i=0,next,first=0,second=1;
+ 	  printf("\n please enter the range num: ");
+ 	  scanf("%d",&num);
+ 	  while (i<num){
+ 	    if (i<=1){
+ 	      next=i; 
+    }
+   else{
+	     next=first+second;
+	     first+second;
+	     second=next;}
+         printf("%d\t",next);
+	     i++;
+	 }
+return 0;
+}
+
+ 

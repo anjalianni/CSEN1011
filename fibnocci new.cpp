3b62dab0753e9f6315c1361bdce2b0e6274b8cf8
Giduturi Sai Sri Anjali <93367806+anjalianni@users.noreply.github.com>
2021-12-30 19:00:22 +0530

    Update fibnocci new.cpp

--- fibnocci new.cpp
+++ fibnocci new.cpp
@@ -18,5 +18,23 @@ int main()
 return 0;
 }
 
+ALGORITHM: 
+ 1. Start
+ 2. Declare variables i, next,first , second
+ 3. Initialize the variables, i=0, second=1, and first =0
+ 4. Enter the number of terms of Fibonacci series to be printed
+ 5. Print First two terms of series
+ 6. Use loop for the following steps
+    -> show=a+b
+    -> a=b
+    -> b=show
+    -> increase value of i each time by 1
+    -> print the value of show
+ 7. End  
+ 
+ LEARNINGS : 
+
+Learnt what a fibnocii series is and how to arrange them.
+
 
  
